66f43861a7c112126f9526b79d2f5e64da48ad4f
Anish Jathan <Anishsjathan@gmail.com>
2022-07-10 17:39:48 +0530

    commit 2

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String args []){
         Circle circle = new Circle("red" , 3);
-        System.out.println(circle.calculateArea());
+        System.out.println(circle.calculatePerimeter());
         circle.display();
 
         Triangle triangle = new Triangle("blue", 1, 3);
-        System.out.println(triangle.calculateArea());
-        circle.display();
+        System.out.println(triangle.calculatePerimeter());
+        triangle.display();
     }
     
 }
